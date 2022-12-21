e448db444c6a1b39aaa910d03b710ec0a013562d
Vineet Pratap Singh <108549821+Vineet9898@users.noreply.github.com>
2022-12-21 11:03:26 +0530

    rating service completed

--- backend/HotelService/src/main/java/com/lcwd/hotel/repositories/HotelRepository.java
+++ backend/HotelService/src/main/java/com/lcwd/hotel/repositories/HotelRepository.java
@@ -1,9 +1,13 @@
 package com.lcwd.hotel.repositories;
-
 import com.lcwd.hotel.entities.Hotel;
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel,String> {
+	
 }
---
+++ backend/RatingService/src/main/java/com/lcwd/rating/service/RatingService.java
@@ -0,0 +1,17 @@
+package com.lcwd.rating.service;
+
+import java.util.List;
+
+import com.lcwd.rating.entities.Rating;
+
+public interface RatingService {
+	
+	//create
+	Rating create(Rating rating);
+	//get all ratings
+	List<Rating> getRatings();
+	//get all ratings by user
+	List<Rating> getRatingByUserId(String userId);
+	//get all by hotel
+	List<Rating>getRatingByHotelId(String hotelId);
+}
