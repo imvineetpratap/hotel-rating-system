b13443566c0f3a782ffcb3e2983666178b1b0c5a
Vineet Pratap Singh <108549821+Vineet9898@users.noreply.github.com>
2022-12-21 22:43:20 +0530

    eureka server

--- backend/UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
+++ backend/UserService/src/main/java/com/lcwd/user/service/UserServiceApplication.java
@@ -2,8 +2,8 @@ package com.lcwd.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
