bba1913db42323ca9e4a812795403a8d460b2635
Vineet Pratap Singh <108549821+Vineet9898@users.noreply.github.com>
2022-12-23 22:14:23 +0530

    instances registered with eureka server of all services

--- backend/RatingService/src/main/java/com/lcwd/rating/entities/Rating.java
+++ backend/RatingService/src/main/java/com/lcwd/rating/entities/Rating.java
@@ -1,11 +1,12 @@
 package com.lcwd.rating.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Entity
 @Getter
