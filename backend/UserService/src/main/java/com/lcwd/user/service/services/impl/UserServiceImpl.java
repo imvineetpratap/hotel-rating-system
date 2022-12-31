a5bd49249ebe364ebe3901c985b6b47d36384f9c
Vineet Pratap Singh <108549821+Vineet9898@users.noreply.github.com>
2022-12-31 23:03:24 +0530

    implementing fiegn client instead of resttemplate to call hotel service

--- backend/UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ backend/UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ import com.lcwd.user.service.entities.Hotel;
 import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exception.ResourceNotFoundException;
+import com.lcwd.user.service.external.services.HotelService;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
 
@@ -12,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -28,7 +27,8 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private RestTemplate restTemplate;
-
+    @Autowired
+private HotelService hotelService;
     private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
@@ -51,16 +51,17 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("user with given id was not found on server!! :" + userId));
         //fetch ratings of the above user from rating service
         // http://192.168.1.4:8083/ratings/users/08ff6c1c-baab-408e-8a72-3c817a575ab7
-        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), Rating[].class);
+      Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), Rating[].class);
         logger.info("{}", ratingsOfUser);
 List<Rating> ratings= Arrays.stream(ratingsOfUser).toList();
 
 List<Rating> ratingList=  ratings.stream().map(rating ->{
             //api call to hotel service to get the hotel
 //          http://localhost:8082/hotels/3b19334d-a249-4092-999f-1134312f4d48
-          ResponseEntity<Hotel> forEntity= restTemplate.getForEntity("http://MY-HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
-         Hotel hotel= forEntity.getBody();
-         logger.info("response status code",forEntity.getStatusCode());
+//          ResponseEntity<Hotel> forEntity= restTemplate.getForEntity("http://MY-HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+//         Hotel hotel= forEntity.getBody(); yeh pehle resttemplate use karke nikal rahe the ab feign se nikalenge
+    Hotel hotel= hotelService.getHotel(rating.getHotelId());
+//         logger.info("response status code",forEntity.getStatusCode());
           //set the hotel to rating
     rating.setHotel(hotel);
     //return the rating
