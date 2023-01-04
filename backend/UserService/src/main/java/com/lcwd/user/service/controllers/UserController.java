eba1d0b108750b92464f1927dbcf49692c2abc8e
Vineet Pratap Singh <108549821+Vineet9898@users.noreply.github.com>
2023-01-05 00:31:17 +0530

    implementation of circuit breaker in user service

--- backend/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ backend/UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.lcwd.user.service.controllers;
 
 import java.util.List;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
 
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
+
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 	//create
 	@PostMapping("/")
 	public ResponseEntity<User> createUser(@RequestBody User user){
@@ -31,10 +37,24 @@ public class UserController {
 	}
 	//single user get
 	@GetMapping("/{userId}")
+	@CircuitBreaker(name="Rating_Hotel_Breaker",fallbackMethod = "ratingHotelFallback")
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId){
-	User user=	userService.getUser(userId);
+		logger.info("Get Single User Handler: UserController");
+		User user=	userService.getUser(userId);
 	return ResponseEntity.ok(user);
 	}
+	//fallback method for circuitbreaker
+	public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+		logger.info("Fallback is executed because service is down : ", ex.getMessage());
+		User user = User.builder()
+				.email("dummy@gmail.com")
+				.name("Dummy")
+				.about("This user is created dummy because some service is down")
+				.userId("141234")
+				.build();
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
 	//all
 	@GetMapping
 	public ResponseEntity<List<User>> getAllUser(){
